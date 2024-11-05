ef122a0e5eebd4615120b9d9ee20e7b0cf715a2d
SchnTgaiSpock <eric.wuh27@gmail.com>
2024-11-05 00:19:24 -0500

    fix

--- src/main/java/io/github/schntgaispock/rrshowcase/core/slimefun/ItemStacks.java
+++ src/main/java/io/github/schntgaispock/rrshowcase/core/slimefun/ItemStacks.java
@@ -86,7 +86,7 @@ public final class ItemStacks {
         "&7Shapeless recipe"
     );
     
-    public static final SlimefunItemStack PIG_IRON = new SlimefunItemStack("SMALL_BLOCK_OF_STEEL", Material.IRON_BLOCK,
+    public static final SlimefunItemStack PIG_IRON = new SlimefunItemStack("PIG_IRON", Material.IRON_BLOCK,
         "&bPig Iron"
     );
     public static final SlimefunItemStack SLIME_INGOT = new SlimefunItemStack("SLIME_INGOT", Material.IRON_INGOT,
